d051dd3bd8c4d2697f1883396f017d7cfc6d2fc2
Recurduck Greco <ilaigreco@gmail.com>
2021-08-30 16:07:02 +0300

    render board and move hero

--- js/game.js
+++ js/game.js
@@ -19,10 +19,9 @@ var gHeroPos
 // Called when game loads
 function init() {
     console.log('init');
-    gHeroPos = { i: 12, j: 5 };
     gBoard = createBoard(14, 14);
-
     console.table(gBoard)
+    renderBoard(gBoard)
 }
 // Create and returns the board with aliens on top, ground at bottom
 function createBoard(rows, cols) {
@@ -30,10 +29,42 @@ function createBoard(rows, cols) {
     for (var j = 2; j < board[0].length-2; j++) {
         board[0][j] = ALIEN
     }
-
-        return board;
-
+    createHero(board)
+    return board;
 }
 
 // Render the board as a <table> to the page
-function renderBoard(board) { }
+function renderBoard(board) {
+    var strHTML = '';
+	for (var i = 0; i < board.length; i++) {
+		strHTML += '<tr>\n';
+		for (var j = 0; j < board[0].length; j++) {
+			var currCell = board[i][j];
+
+			var cellClass = getClassName({ i: i, j: j })
+			// if (currCell.type === FLOOR) cellClass += ' floor';
+			// else if (currCell.type === WALL) cellClass += ' wall';
+			strHTML += `\t<td class="cell ${cellClass}">\n`;
+			switch(currCell) {
+                case HERO: 
+                    strHTML += HERO; 
+                    break;
+                case ALIEN: 
+                    strHTML += ALIEN;
+                    break;
+                default:
+                    strHTML += ' ';
+            }
+			strHTML += '\t</td>\n';
+		}
+		strHTML += '</tr>\n';
+	}
+	var elBoard = document.querySelector('.board');
+	elBoard.innerHTML = strHTML;
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+	var cellClass = 'cell-' + location.i + '-' + location.j;
+	return cellClass;
+}
--- js/hero.js
+++ js/hero.js
@@ -1,13 +1,35 @@
 const LASER_SPEED = 80;
-var gHero = {pos: {i:12, j: 5}, isShoot: false};
+var gHero = { pos: { i: 12, j: 5 }, isShoot: false };
 
 // creates the hero and place it on board
-function createHero(board) {}
+function createHero(board) {
+    console.log('hero');
+    board[gHero.pos.i][gHero.pos.j] = HERO
+}
 // Handle game keys
-function onKeyDown(ev) {}
+function onKeyDown(ev) {
+    console.log(ev);
+    var i = gHero.pos.i;
+    var j = gHero.pos.j;
+    switch (ev.key) {
+        case 'ArrowLeft':
+            moveHero({ i, j: j - 1 });
+            break;
+        case 'ArrowRight':
+            moveHero({ i, j: j + 1 });
+            break;
+    }
+
+}
 // Move the hero right (1) or left (-1)
-function moveHero(dir) {}
+function moveHero(dir) {
+    gBoard[gHero.pos.i][gHero.pos.j] = ''
+    gHero.pos = dir
+    console.log(gHero.pos);
+    gBoard[gHero.pos.i][gHero.pos.j] = HERO
+    renderBoard(gBoard)
+}
 // renders a LASER at specific cell for short time and removes it
-function blinkLaser(pos) {}
+function blinkLaser(pos) { }
 // Sets an interval for shutting (blinking) the laser up towards aliens
-function shoot() {}
+function shoot() { }
